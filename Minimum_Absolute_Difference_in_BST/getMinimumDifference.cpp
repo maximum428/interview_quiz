54dae37b9898ee91c904eab0f8943e7033dffd4f
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-01-19 23:17:10 -0800

    Create getMinimumDifference.cpp

---
+++ Minimum_Absolute_Difference_in_BST/getMinimumDifference.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        _prev = nullptr;
+        min_diff = INT_MAX;
+        inorder(root);
+        return min_diff;
+    }
+    
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        if (_prev) min_diff = min(min_diff, root->val - *_prev);
+        _prev = &root->val;
+        inorder(root->right);
+    }
+private:
+    int* _prev;
+    int min_diff;
+};
