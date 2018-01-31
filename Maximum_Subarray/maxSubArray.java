e501acb5342ea39e2d4aa1206cb33954b08cdcc2
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-01-31 11:17:42 -0800

    Update maxSubArray.java

--- Maximum_Subarray/maxSubArray.java
+++ Maximum_Subarray/maxSubArray.java
@@ -1,12 +1,24 @@
 import java.util.Arrays;
 
 class Solution {
-    public int maxSubArray(int[] nums) {
+    public int maxSubArray1(int[] nums) {
         int[] f = new int[nums.length];
         f[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
             f[i] = Math.max(nums[i] + f[i - 1], nums[i]);
         }
         return Arrays.stream(f).summaryStatistics().getMax();
+    }
+    
+    public int maxSubArray2(int[] nums) {
+        //int[] f = new int[nums.length];
+        //f[0] = nums[0];
+        int max = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(nums[i] + sum, nums[i]);
+            max = Math.max(sum, max);
+        }
+        return max;
     }
 }
