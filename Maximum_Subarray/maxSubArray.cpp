69d1abccedfc5acea1e4ed1b5d7f2ff5a8da8b80
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-01-31 11:14:11 -0800

    Update maxSubArray.cpp

--- Maximum_Subarray/maxSubArray.cpp
+++ Maximum_Subarray/maxSubArray.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray1(vector<int>& nums) {
         vector<int> f(nums.size());
         f[0] = nums[0];
         for (int i = 1; i < nums.size(); i++) {
             f[i] = max(nums[i] + f[i - 1], nums[i]);
         }
         return *std::max_element(f.begin(), f.end());
+    }
+    
+    int maxSubArray2(vector<int>& nums) {
+        int m = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            sum = max(nums[i] + sum, nums[i]);
+            m = max(m, sum);
+        }
+        return m;
     }
 };
