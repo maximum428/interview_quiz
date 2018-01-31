7e903358bfe05ceba54cb045064d3b8828660602
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-01-31 11:09:40 -0800

    Update maxSubArray.c

--- Maximum_Subarray/maxSubArray.c
+++ Maximum_Subarray/maxSubArray.c
@@ -3,7 +3,7 @@
 #define MAX(a, b) (((a)>(b))?(a):(b))
 #define MIN(a, b) (((a)<(b))?(a):(b))
 
-int maxSubArray(int* nums, int numsSize) {
+int maxSubArray1(int* nums, int numsSize) {
     int i, max;
     int f[numsSize];
     
@@ -17,8 +17,21 @@ int maxSubArray(int* nums, int numsSize) {
     return max;
 }
 
+int maxSubArray2(int* nums, int numsSize) {
+    int i, max, sum;
+    
+    sum = nums[0];
+    max = nums[0];
+    for (i = 1; i < numsSize; i++) {
+        sum = MAX(nums[i] + sum, nums[i]);
+        max = MAX(max, sum);
+    }
+    
+    return max;
+}
+
 int main(void) {
     int nums[9] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    printf("%d\n",maxSubArray(nums, sizeof(nums)/sizeof(nums[0])));
+    printf("%d\n",maxSubArray1(nums, sizeof(nums)/sizeof(nums[0])));
     return 0;
 }
