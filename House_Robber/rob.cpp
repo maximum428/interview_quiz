d07b2921b0dc1e1fabc7f1370b16dafb054ae96c
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-02-01 23:53:40 -0800

    Update rob.cpp

--- House_Robber/rob.cpp
+++ House_Robber/rob.cpp
@@ -16,6 +16,17 @@ public:
         }
         return max(rob, no_rob);
     }
+    int rob3(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int dp2 = 0;
+        int dp1 = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            int dp = max(dp2 + nums[i], dp1);
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp1;
+    }
 private:
     int rob1(const vector<int>& nums, int i) {
         if (i < 0) return 0;
