ea010f6e09d6cac91f8f34aca55bcabc4ae33387
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-02-01 23:46:30 -0800

    Update rob.cpp

--- House_Robber/rob.cpp
+++ House_Robber/rob.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int rob1(vector<int>& nums) {
+        const int n = nums.size();
+        m_ = vector<int>(n, -1);
+        return rob1(nums, n - 1);
+    }
+    int rob2(vector<int>& nums) {
         int rob = 0;
         int no_rob = 0;
         
@@ -11,4 +16,11 @@ public:
         }
         return max(rob, no_rob);
     }
+private:
+    int rob1(const vector<int>& nums, int i) {
+        if (i < 0) return 0;
+        if (m_[i] >= 0) return m_[i];
+        return m_[i] = max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+    }
+    vector<int> m_;
 };
