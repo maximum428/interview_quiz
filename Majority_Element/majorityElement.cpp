1c4bd24e4d353e9eeab1851f1e6e2943b1173cfa
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-02-13 14:47:24 -0800

    Create majorityElement.cpp

---
+++ Majority_Element/majorityElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {\
+        int majority = nums[0];
+        int res = nums.size() / 2;
+        unordered_map<int, int> counts;
+                                            
+        if (nums.size() == 0) throw invalid_argument("Error: Incorrect data");
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (++counts[nums[i]] > res)
+                return nums[i];
+        }
+        return majority;
+    }
+};
