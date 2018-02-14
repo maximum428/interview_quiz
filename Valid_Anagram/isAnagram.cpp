d94bb1dcaa87d3fb15ed895b3cef1be4a7e19860
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-02-13 17:11:52 -0800

    Create isAnagram.cpp

---
+++ Valid_Anagram/isAnagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int counts[26] = {0};
+        
+        for (int i = 0; i < s.size(); i++) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (counts[i]) return false;
+        }
+        return true;
+    }
+};
