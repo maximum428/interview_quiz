3d7c2a1956d71ed37141270be4468665ecba2133
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-02-15 16:09:02 -0800

    Update reverse.cpp

--- Reverse_Integer/reverse.cpp
+++ Reverse_Integer/reverse.cpp
@@ -8,4 +8,16 @@ public:
         }
         return (res<INT_MIN || res>INT_MAX) ? 0 : res;
     }
+    int reverse2(int x) {
+        int result = 0;
+        while (x) {
+            int tail = x % 10;
+            int tmp = result * 10 + tail;
+            if ((tmp - tail) / 10 != result)
+                return 0;
+            result = tmp;
+            x /= 10;
+        }
+        return result;
+    }
 };
