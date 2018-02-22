656b68779c8bd44f39bc8859937445c46aa87954
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-02-22 10:19:24 -0800

    Create quickSort.cpp

---
+++ Quick_Sort/quickSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void quickSort(int *arr, int left, int right) {
+    int i = left, j = right, tmp;
+    int pivot = arr[(left + right) / 2];
+    
+    while (i <= j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+        
+        if (i <= j) {
+            tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+    
+    if (left < j) quickSort(arr, left, j);
+    if (i < right) quickSort(arr, i, right);
+}
