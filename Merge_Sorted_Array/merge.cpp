60cef81d653d0c5c8603a81d0de51f5632326f91
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-03-01 13:26:53 -0800

    Update merge.cpp

--- Merge_Sorted_Array/merge.cpp
+++ Merge_Sorted_Array/merge.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void merge1(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m - 1, j = n - 1, k = m + n - 1;
+        nums1.resize(m+n);
         while (j >= 0) {
             nums1[k--] = i >= 0 && nums1[i] >= nums2[j] ? nums1[i--] : nums2[j--];
         }
