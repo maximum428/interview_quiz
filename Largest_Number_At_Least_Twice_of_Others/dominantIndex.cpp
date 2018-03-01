c1f8d6aee33921bf63eaedc6f88700d1e428ecd0
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-03-01 15:24:06 -0800

    Update dominantIndex.cpp

--- Largest_Number_At_Least_Twice_of_Others/dominantIndex.cpp
+++ Largest_Number_At_Least_Twice_of_Others/dominantIndex.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dominantIndex(vector<int>& nums) {
+    int dominantIndex1(vector<int>& nums) {
         int maxOne = 0, maxTwo = 0, index;
         
         for (int i = 0; i < nums.size(); i++) {
@@ -13,5 +13,16 @@ public:
             }
         }
         return maxOne >= maxTwo * 2 ? index : -1;
+    }
+    
+    int dominantIndex2(vector<int>& nums) {
+        if (nums.size() <= 1) return -1;
+        vector<int>::iterator max_elm = max_element(nums.begin(), nums.end());
+        int index = distance(nums.begin(), max_elm);
+        for (int i = 0; i < nums.size(); i++) {
+            if (i != index && nums[i]*2 > *max_elm)
+                return -1;
+        }
+        return index;
     }
 };
