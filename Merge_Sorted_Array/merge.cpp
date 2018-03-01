0b851b5fcf81a4b504ea422f27f321f6361c4389
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-03-01 13:32:24 -0800

    Update merge.cpp

--- Merge_Sorted_Array/merge.cpp
+++ Merge_Sorted_Array/merge.cpp
@@ -12,11 +12,7 @@ public:
         int i = m - 1, j = n - 1, k = m + n - 1;
         nums1.resize(m+n);
         while (i >= 0 && j >= 0) {
-            if (nums1[i] >= nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else if (nums1[i] < nums2[j]) {
-                nums1[k--] = nums2[j--];
-            } 
+            nums1[k--] = nums1[i] >= nums2[j] ? nums1[i--] : nums2[j--]; 
         }
         while (i >= 0) {
             nums1[k--] = nums1[i--];
