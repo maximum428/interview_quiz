db7dc403725b826d52bd557633c9794337807085
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-03-19 23:19:33 -0700

    Update isAnagram.cpp

--- Valid_Anagram/isAnagram.cpp
+++ Valid_Anagram/isAnagram.cpp
@@ -14,4 +14,22 @@ public:
         }
         return true;
     }
+    
+    bool isAnagram2(string s, string t) {
+        if (s.size() != t.size()) return false;
+    
+        int m = 0, n = 0;
+        for (int i = 0; i < s.size(); i++) {
+            m ^= s[i];
+            n ^= t[i];
+        }
+        /*
+        for (auto c : s) {
+            m ^= c;
+        }
+        for (auto c : t) {
+            n ^= c;
+        } */
+        return m == n;
+    }
 };
