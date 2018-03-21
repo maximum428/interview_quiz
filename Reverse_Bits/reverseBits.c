8e13397cc1fe5c38d755ea5eced723dcea750b10
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-03-20 21:19:35 -0700

    Update reverseBits.c

--- Reverse_Bits/reverseBits.c
+++ Reverse_Bits/reverseBits.c
@@ -10,10 +10,10 @@ uint32_t reverseBits(uint32_t n) {
 }
 uint32_t reverseBits(uint32_t n) {
    n = (n & 0xFFFF0000) >> 16 | (n & 0x0000FFFF) << 16;
-   n = (n & 0xFF00) >> 8 | (n & 0x00FF) << 8;
-   n = (n & 0xF0) >> 4 | (n & 0x0F) << 4;
-   n = (n & 0xCC) >> 2 | (n & 0x33) << 2;
-   n = (n & 0xAA) >> 1 | (n & 0x55) << 1;
+   n = (n & 0xFF00FF00) >> 8 | (n & 0x00FF00FF) << 8;
+   n = (n & 0xF0F0F0F0) >> 4 | (n & 0x0F0F0F0F) << 4;
+   n = (n & 0xCCCCCCCC) >> 2 | (n & 0x33333333) << 2;
+   n = (n & 0xAAAAAAAA) >> 1 | (n & 0x55555555) << 1;
 }
 
 int main(void) {
