23a262770d9dc7d538c79167a9096a92f126fecb
maximum428 <35505967+maximum428@users.noreply.github.com>
2018-04-04 14:23:13 -0700

    Update numMatchingSubseq.cpp

--- Number_Of_Matching_Subsequence/numMatchingSubseq.cpp
+++ Number_Of_Matching_Subsequence/numMatchingSubseq.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    int numMatchingSubseq(string S, vector<string>& words) {
+    int numMatchingSubseq1(string S, vector<string>& words) {
         vector<vector<int>> pos(128);
         for (int i = 0; i < S.length(); i++)
             pos[S[i]].push_back(i);
         int ans = 0;
         for (const string& word : words)
-            ans += isMatch(word, pos);
+            ans += isMatch1(word, pos);
+        return ans;
+    }
+    
+    int numMatchingSubseq2(string S, vector<string>& words) {
+        map<char, vector<int>> pos;
+        for (int i = 0; i < S.length(); i++)
+            pos[S[i]].push_back(i);
+        
+        int ans = 0;
+        for (auto word : words)
+            ans += isMatch2(word, pos);
         return ans;
     }
     
 private:
     unordered_map<string, bool> m_;
-    bool isMatch(const string& word, const vector<vector<int>>& pos) {
+    bool isMatch1(const string& word, const vector<vector<int>>& pos) {
         if (m_.count(word)) return m_[word];
         int last_index = -1;
         for (const char c : word) {
@@ -22,5 +33,16 @@ private:
             last_index = *it;
         }
         return m_[word] = true;
+    }
+    
+    bool isMatch2(const string& word, map<char, vector<int>>& pos) {
+        int last_index = -1;
+        for (auto c : word) {
+            const vector<int>& p = pos[c];
+            const auto it = lower_bound(p.begin(), p.end(), last_index + 1);
+            if (it == p.end()) return false;
+            last_index = *it;
+        }
+        return true;
     }
 };
