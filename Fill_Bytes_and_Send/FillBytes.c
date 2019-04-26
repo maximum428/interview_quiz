f43a91b3f1a165bab47db2dd92da97f7c917b6b0
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-04-26 16:02:47 -0700

    Update FillBytes.c

--- Fill_Bytes_and_Send/FillBytes.c
+++ Fill_Bytes_and_Send/FillBytes.c
@@ -32,9 +32,30 @@ void receive(uint8_t *bytes, uint8_t nbytes) {
     }
 }
 
+void method1(Data *data) {
+    ssize_t nr = fread(data, 1, sizeof(Data), fd);
+    while (nr < sizeof(Data) {
+        ssize_t rr = fread(data+nr, 1, sizeof(Data)-nr, fd);
+        if (rr >= 0)
+            nr += rr;
+    }
+    sendData(data);
+}
+           
+void method2(Data *data) {
+    ssize nr = 0;
+    while (sizeof(data) - nr) {
+        nr += MIN(fread(data+nr, 1, sizeof(Data)-nr, fd), sizeof(Data) - nr);
+    }
+}
+
 int main(void) {
     uint8_t *bytes = malloc(16 * sizeof(uint8_t));
     
     receive(&bytes, 8);
+    
+    Data *data;
+    method1(data);
+    method2(data);
     return 0;
 }
