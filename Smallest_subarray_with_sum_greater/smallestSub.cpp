0b1f2850dfc879341fdcbd859d1e871b088f31f5
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-04-30 09:59:12 -0700

    Smallest subarray with sum greater a given value
    Given an array of integers and a number x, find the smallest subarray with sum greater than the given value.
    Examples:
    arr[] = {1, 4, 45, 6, 0, 19}
       x  =  51
    Output: 3
    Minimum length subarray is {4, 45, 6}
    
    arr[] = {1, 10, 5, 2, 7}
       x  = 9
    Output: 1
    Minimum length subarray is {10}
    
    arr[] = {1, 11, 100, 1, 0, 200, 3, 2, 1, 250}
        x = 280
    Output: 4
    Minimum length subarray is {100, 1, 0, 200}
    
    arr[] = {1, 2, 4}
        x = 8
    Output : Not Possible
    Whole array sum is smaller than 8.

---
+++ Smallest_subarray_with_sum_greater/smallestSub.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int smallestSubarray(vector<int> vec, int x) {
+    int len = vec.size();
+    int curr_sum = 0, min_len = len + 1, start = 0, end = 0;
+    while (end < len) {
+        while (curr_sum <= x && end < len) {
+            curr_sum += vec[end++];
+        }
+        
+        while (curr_sum > x && start < len) {
+            if (end - start < min_len) {
+                min_len = end - start;
+            }
+            
+            curr_sum -= vec[start++];
+        }
+    }
+    return min_len;
+}
+
+int main() {
+    vector<int> vec = { 1, 4, 45, 6, 10, 19 };
+    cout << smallestSubarray(vec, 51) << endl;
+}
