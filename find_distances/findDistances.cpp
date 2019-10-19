1caa2870f611035934c25087f4afe2089e64c021
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-10-19 11:06:32 -0700

    Create findDistances.cpp

---
+++ find_distances/findDistances.cpp
@@ -0,0 +1,28 @@
+vector<int> findDistance(vector<char>& vec, char k) {
+    int start = 0;
+    vector<int> res(vec.size());
+    
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] == k) {
+            for (int j = start; j <= i; j++) {
+                if (start == 0)
+                    res[j] = abs(i-j);
+                else
+                    res[j] = min(abs(j-start), abs(i-j));
+            }
+            start = i;
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<char> vec = {'f', 'r', 'e', 'e', 'w', 'i', 'r', 'e'};
+    char k = 'e';
+    
+    vector<int> res = findDistance(vec, k);
+    for (auto e : res) {
+        cout << e << " ";
+    }
+    cout << "\n";
+}
