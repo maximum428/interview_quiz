56d585563941a4bf83ef751836fb53e322198f80
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-10-19 20:59:02 -0700

    Create 4sum.cpp

---
+++ 3Sum_4Sum/4sum.cpp
@@ -0,0 +1,28 @@
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> res;
+    if (nums.size() < 4) return res;
+    
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < nums.size() - 3; i++) {
+        if (i > 0 && nums[i] == nums[i-1]) continue;
+        for (int j = i + 1; j < nums.size() - 2; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            int third = j + 1, four = nums.size() - 1;
+            while (third < four) {
+                int sum = nums[i] + nums[j] + nums[third] + nums[four];
+                if (sum < target)
+                    third--;
+                else if (sum > target)
+                    four--;
+                else {
+                    vector<int> m = {nums[i], nums[j], nums[third], nums[four]};
+                    res.push_back(m);
+                    while (third < four && nums[third] == nums[third+1]) third++;
+                    while (third < four && nums[four] == nums[four-1]) four--;
+                }
+            }
+        }
+    }
+    
+}
