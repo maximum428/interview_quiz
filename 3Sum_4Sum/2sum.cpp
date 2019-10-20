e3d43ad187d73f6f2e98678d04ea1bc6edcf3704
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-10-20 01:54:24 -0700

    Create 2sum.cpp

---
+++ 3Sum_4Sum/2sum.cpp
@@ -0,0 +1,13 @@
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> mp;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int compliment = target - nums[i];
+            if (mp.find(complement) != mp.end())
+                return {mp[compliment], i]}
+            mp[nums[i]] = i;
+        }
+        
+        return {};
+    }
