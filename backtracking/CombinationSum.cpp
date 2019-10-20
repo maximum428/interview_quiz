47b47bc6e1eb7de3123db2c5a85885302e211ee1
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-10-19 23:36:46 -0700

    Create CombinationSum.cpp

---
+++ backtracking/CombinationSum.cpp
@@ -0,0 +1,18 @@
+void backtrack(vector<int>& candidates, vector<vector<int>>& res, vector<int>& nums, int target, int start) {
+        if (!target) {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = start; i < candidates.size() && target >= candidates[i]; i++) {
+            nums.push_back(candidates[i]);
+            backtrack(candidates, res, nums, target - candidates[i], i);
+            nums.pop_back();
+        }
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> nums;
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, res, nums, target, 0);
+        return res;
+}
