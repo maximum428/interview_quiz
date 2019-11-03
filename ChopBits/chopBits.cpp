c81fccf5a9eada240436a295487b0741a1676be3
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-11-03 10:18:16 -0800

    Create chopBits.cpp

---
+++ ChopBits/chopBits.cpp
@@ -0,0 +1,11 @@
+/* Given a number, get the bits on given position and length.
+For example:   n = 01101001, pos = 3, len = 4,
+               return 1101
+*/
+
+uint32_t chopBits(uint32_t n, uint32_t pos, uint32_t len) {
+    int32_t mask = -1;    // -1 = 11111....11111;
+    mask = ~(mask << len);  // same as mask <<= len; mask = ~mask;
+    n >>= pos;
+    return n & mask;
+}
