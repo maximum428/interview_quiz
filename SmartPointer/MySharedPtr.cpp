73230bc585fdbcd3c380981c41ed5412815929ce
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-11-14 10:39:23 -0800

    Create MySharedPtr.cpp

---
+++ SmartPointer/MySharedPtr.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+template<typename T>
+class MySharedPtr {
+public:
+   MySharedPtr(T *p = NULL) {
+      ptr = p;
+      count = new int(1);
+   }
+   MySharedPtr(MySharedPtr<T> &p) {
+      ptr = p.ptr;
+      *(p.count) = *(p.count) + 1;
+      count = p.count;
+   }
+   MySharedPtr& operator=(MySharedPtr<T> &p) {
+      ptr = p.ptr;
+      *(p.count) = *(p.count) + 1;
+      count = p.count;
+      return *this;
+   }
+   ~MySharedPtr() {
+      (*count)--;
+      if (*count == 0) {
+         cout << "delete .... " << endl;
+         delete(ptr);
+         delete(count);
+      }
+   }
+private:
+   T *ptr;
+   int *count;
+};
+int main() {
+   MySharedPtr<int> myptr(new int(10));
+   MySharedPtr<int> a(myptr);
+   MySharedPtr<int> b;
+   b = myptr;
+}
