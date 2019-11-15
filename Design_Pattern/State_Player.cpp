6f87a8225dcb6dcb523a05ae27b23b15188ee89a
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-11-14 21:53:12 -0800

    Create State_Player.cpp

---
+++ Design_Pattern/State_Player.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+using namespace std;
+
+class MusicPlayerState {
+public:
+    MusicPlayerState(string name) : m_name(name) {}
+    virtual ~MusicPlayerState() {}
+    virtual void Play(MusicPlayer *player) {
+        cout << "Illegal state transition from " << GetName() << " to Playing\n";
+    }
+    virtual void Pause(MusicPlayer *player) {
+        cout << "Illegal state transition from " << GetName() << " to Paused\n";
+    }
+    virtual void Stop(MusicPlayer *player) {
+        cout << "Illegal state transition from " << GetName() << " to Stopped\n";
+    }
+    string GetName() {
+        return m_name;
+    }
+private:
+    string m_name;
+};
+
+class PlayState : public MusicPlayerState {
+    PlayingState() : MusticPlayerState(string("Playing")) {}
+    virtual ~PlayingState() {}
+    virtual void Pause(MusicPlayer *player) {
+        player->setState(MusicPlayer::ST_PAUSED);
+    }
+    virtual void Stop(MusicPlayer *player) {
+        player->SetState(MusicPlayer::ST_STOPPED);
+    }
+};
+
+class PauseState : public MusicPlayerState {
+public:
+    PausedState();
+    virtual ~PausedState();
+    
+    virtual void Play(MusicPlayer *player) {
+        player->SetState(MusicPlayer::ST_PLAYING);
+    }
+    virtual void Stop(MusicPlayer *player) {
+        player->SetState(MusicPlayer::ST_STOPPED);
+    }
+};
+
+class StoppedState : public MusicPlayerState {
+public:
+    StoppedState() : MusticPlayerState(string("Stopped")) {}
+    virtual ~StoppedState() {}
+    virtual void Play(MusticPlayer *player) {
+        player->SetState(MusicPlayer::ST_PLAYING);
+    }
+};
+
+class MusicPlayer {
+public:
+    enum State { ST_STOPPED, ST_PLAYING, ST_PAUSED };
+    MusicePlayer() : m_pState(new StoppedState()) {}
+    virtural ~MusicPlayer() {
+        delete m_pState;
+    }
+    void Play() {
+        m_pState->Play(this);
+    }
+    void Pause() {
+        m_pState->Pause(this);
+    }
+    void Stop() {
+        m_pState->Stop(this);
+    }
+    void SetState(State state) {
+        cout << "changing from " << m_pState->GetName() << " to ";
+        delete m_pState;
+        
+        if (state == ST_STOPPED) {
+            m_pState = new StoppedState();
+        } else if (state == ST_PLAYING) {
+            m_pState = new PlayingState();
+        } else {
+            m_pState = new PausedState();
+        }
+        cout << m_pState->GetName() << " state\n";
+    }
+private:
+    MusicPlayerState *m_pState;
+};
+
+int main() {
+    MusicPlayer player;
+    player.Play();
+    player.Pause();
+    player.Stop();
+    
+    return 0;
+}
