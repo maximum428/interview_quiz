c4eb57a6c588eb23b0d26f11b9cdd34fdafbab87
maximum428 <35505967+maximum428@users.noreply.github.com>
2019-11-16 19:10:53 -0800

    Update chopBits.cpp

--- ChopBits/chopBits.cpp
+++ ChopBits/chopBits.cpp
@@ -4,8 +4,10 @@ For example:   n = 01101001, pos = 3, len = 4,
 */
 
 uint32_t chopBits(uint32_t n, uint32_t pos, uint32_t len) {
+    return (((1 << len) - 1) & (n >> (pos - 1)));
+    /*
     int32_t mask = -1;    // -1 = 11111....11111;
     mask = ~(mask << len);  // same as mask <<= len; mask = ~mask;
     n >>= pos;
-    return n & mask;
+    return n & mask; */
 }
