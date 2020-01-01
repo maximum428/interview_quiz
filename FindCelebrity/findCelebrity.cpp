23e047593aed09f0f7edc4b3c5b77276b24b5208
maximum428 <35505967+maximum428@users.noreply.github.com>
2020-01-01 14:32:49 -0800

    Update findCelebrity.cpp

--- FindCelebrity/findCelebrity.cpp
+++ FindCelebrity/findCelebrity.cpp
@@ -3,10 +3,12 @@ class Solution {
 public:
     int findCelebrity(int n) {
         for (int i = 0, j = 0; i < n; i++) {
-            if (i != j && (knows(i, j) || !(know(j, i)))
-                break;
+            for (j = 0; j < n; j++) {
+                if (i != j && (knows(i, j) || !(know(j, i)))
+                    break;
+            }
+            if (j == n) return i;
         }
-        if (j == n) return i;
     }
     return -1;
 };
