08d1aebc7ecf33bd889716335b9a6f02f742c6ab
maximum428 <35505967+maximum428@users.noreply.github.com>
2020-01-01 14:36:52 -0800

    Update findCelebrity.cpp

--- FindCelebrity/findCelebrity.cpp
+++ FindCelebrity/findCelebrity.cpp
@@ -2,6 +2,22 @@ bool knows(int a, int b);
 class Solution {
 public:
     int findCelebrity(int n) {
+#if 1
+        vector<bool> candidates(n, true);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (candidates[i] && i != j) {
+                    if (knows(i, j) || !know(j, i)) {
+                        candidates[i] = false;
+                        break;
+                    } else {
+                        candidates[j] = false;
+                    }
+                }
+            }
+            if (candidates[i]) return i;
+        }
+#else
         for (int i = 0, j = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
                 if (i != j && (knows(i, j) || !(know(j, i)))
@@ -10,5 +26,6 @@ public:
             if (j == n) return i;
         }
     }
+#endif
     return -1;
 };
