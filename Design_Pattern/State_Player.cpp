36c73cd83d5e581ff65c0874f39c65bd28570d23
maximum428 <35505967+maximum428@users.noreply.github.com>
2020-01-29 19:46:04 -0800

    Update State_Player.cpp

--- Design_Pattern/State_Player.cpp
+++ Design_Pattern/State_Player.cpp
@@ -1,3 +1,6 @@
+/*
+The State Pattern allows an object to alter its behavior when its internal state changes. The object will appear to change its class.
+*/
 #include <iostream>
 
 using namespace std;
