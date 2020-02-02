7b383f4430c0cabec79fa19078f768913da16164
maximum428 <35505967+maximum428@users.noreply.github.com>
2020-02-02 12:32:17 -0800

    Update AbstractFactory_Burger.cpp

--- Design_Pattern/AbstractFactory_Burger.cpp
+++ Design_Pattern/AbstractFactory_Burger.cpp
@@ -1,3 +1,7 @@
+/*
+"The Abstract Factory Pattern provides an interface for creating families of related or dependent 
+objects without specifying their concrete classes"
+*/
 #include <iostream>
 
 using namespace std;
