f100d64eeade2243bf056c5e6525d175fa356a96
maximum428 <35505967+maximum428@users.noreply.github.com>
2020-02-04 14:05:57 -0800

    Create MyUniquePtr.cpp

---
+++ SmartPointer/MyUniquePtr.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class MyUniquePtr {
+public:
+    MyUniquePtr(T *p = NULL) {
+        ptr = p;
+    }
+    MyUniquePtr(const MyUniquePtr<T> &p) {
+        ptr = p.ptr;
+        p->ptr = nullptr;
+    }
+    MyUniquePtr& operator=(const MyUniquePtr<T> &p) {
+        if (this == p) return *this;
+        ptr = p->ptr;
+        return *this;
+    }
+    ~MyUniquePtr() {
+        delete(ptr);
+    }
+private:
+    T *ptr;
+};
+
+int main() {
+    MyUniquePtr<int> myptr(new int(10));
+    
+}
