272b6a7e5381cf0f2bbf2179dc18d0da1a5223b9
maximum428 <35505967+maximum428@users.noreply.github.com>
2020-10-24 20:52:10 -0700

    Update stack_queue.cpp

--- implement_Stack/stack_queue.cpp
+++ implement_Stack/stack_queue.cpp
@@ -3,6 +3,37 @@
 
 using namespace std;
 
+#if 1
+template<typename T>
+class Queue {
+    vector<T> q;
+public:
+    void push(T val) {
+        q.push_back(val);
+    }
+    T front() {
+        if (q.size() == 0) return NULL;  // using nullptr will build failed
+        return q[0];
+    }
+    void pop() {
+        if (q.size() == 0) return;
+        q.erase(q.begin());
+    }
+    int size() {
+        return q.size();
+    }
+    bool empty() {
+        if (q.size() == 0) return true;
+        return false;
+    }
+    void remove(T val) {
+        if (q.size() == 0) return;
+        typename vector<T>::iterator it = find(q.begin(), q.end(), val);
+        if (it != q.end())
+            q.erase(it);
+    }
+};
+#else
 class Queue {
 public:
     void push(int x) {
@@ -31,7 +62,33 @@ public:
 private:
     vector<int> q;
 };
+#endif
 
+#if 1
+template<typename T>
+class Stack {
+    vector<T> stk;
+public:
+    void push(T val) {
+        //typename vector<T>::iterator it = stk.begin();
+        //stk.insert(it, val);
+        stk.insert(stk.begin(), val);
+    }
+    void pop() {
+        stk.erase(stk.begin());
+    }
+    T top() {
+        if (stk.size() == 0) return NULL; //  using nullptr will cause error.
+        rturn stk[0];
+    }
+    void remove(T val) {
+        if (stk.size() == 0) return;
+        typename vector<T>::iterator it = find(stk.begin(), stk.end(), val);
+        if (it != stk.end())
+            stk.erase(it);
+    }
+};
+#else
 class Stack {
 public:
     void push(int x) {
@@ -56,6 +113,7 @@ public:
 private:
     vector<int> stk;
 };
+#endif
 
 int main() {
     Queue queue = Queue();
