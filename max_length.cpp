ff536226f5f1b1a805fa00ddbda9a88462ee5d1c
maximum428 <35505967+maximum428@users.noreply.github.com>
2024-06-02 21:32:40 -0700

    The maximum length of a subsequence chosen form the tickets

---
+++ max_length.cpp
@@ -0,0 +1,2 @@
+Consider an array of n ticket prices, tickets. A number, m, is defined as the size of some subsequence of tickets, s, where each element covers an unbroken range of integers.  
+That is, if the elements in s are sorted, the absolute difference between any elements j and j + 1 is either 0 or 1.  Determine the maximum length of a subsquence chosen from the tickets array.
