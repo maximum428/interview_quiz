7c22860d1152b34afd76c17d3b407b4c06b9418b
maximum428 <35505967+maximum428@users.noreply.github.com>
2024-06-03 10:00:05 -0700

    Update countWords.cpp

--- Count words/countWords.cpp
+++ Count words/countWords.cpp
@@ -1,1 +1,30 @@
-
+int countWords(const string& sentence) {
+    int wordcount = 0;
+    bool inword = false;
+    
+    if (sentence.empty()) {
+        return wordcount;
+    }
+    
+    for (const char& ch : sentence) {
+        if (isspace(ch)) {
+            if (inWord) {
+                inWord = false;
+                wordcount++;
+            }
+        } else if (isalpha(ch) || ch == '-') {
+            inWord = true;
+        } else if (ch == '.' || ch == ',' || ch == '?' || ch == '!') {
+            if (inWord) {
+                wordcoun++;
+                inWord = false;
+            }
+        }
+    }
+    
+    if (inWord) {
+        wordcount++;
+    }
+    
+    return wordcount;
+}
