b2fe14f03b3ce4d9196a8455c10c5ca689392635
maximum428 <35505967+maximum428@users.noreply.github.com>
2024-06-06 11:05:47 -0700

    Update max_sequence.cpp

--- maximum sequence length/max_sequence.cpp
+++ maximum sequence length/max_sequence.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+int MaxSequence(vector<int>& nums) {
+    if (nums.size() < 2) return nums.size();
+    
+    sort(nums.begin(), nums.end());
+    int res = 0, subseq_length = 1;
+    
+    for (auto i = 0; i < nums.size() - 1; i++) {
+        if (nums.at(i) == nums.at(i+1) || nums.at(i) + 1 == nums.at(i+1)) {
+            subseq_length++;
+        } else {
+            res = max(res, subseq_length);
+            subseq_length = 1;
+        }
+    }
+    
+    return res;
+    
+}
+
+int main()
+{
+    std::vector<int> vec = {5, 4, 3, 2, 1, 8, 8, 8, 9};
+    cout << "maxmum length: " << MaxSequence(vec) << endl;
+    
+    return 0;
+}
