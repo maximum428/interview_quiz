da56f30016112cca5d6956b6392ec6f1031c1d96
maximum428 <35505967+maximum428@users.noreply.github.com>
2024-10-06 11:17:11 -0700

    Update floodFill.cpp

--- Flood_Fill/floodFill.cpp
+++ Flood_Fill/floodFill.cpp
@@ -17,3 +17,44 @@ private:
         startFloodFill(image, sr + 1, sc, color, newColor);
     }
 };
+
+void fillFlood2(std::vector<std::vector<int>>& image, int sr, int sc, int color, int newColor) {
+    std::queue<int> sr_q, sc_q;
+    sr_q.push(sr);
+    sc_q.push(sc);
+    
+    std::vector<std::vector<int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    
+    while (!sr_q.empty() && !sc_q.empty()) {
+        int r = sr_q.front();
+        sr_q.pop();
+        int c = sc_q.front();
+        sc_q.pop();
+        
+        for (auto dir = 0; dir < dirs.size(); dir++) {
+            if ((r + dirs[dir][0]) < 0 || (c + dirs[dir][1]) < 0 || (r + dirs[dir][0]) >= image.size() || (c + dirs[dir][1]) >= image[0].size() || image[r+dirs[dir][0]][c+dirs[dir][1]] != color) { 
+                continue;
+            }
+            sr_q.push(r+dirs[dir][0]);
+            sc_q.push(c+dirs[dir][1]);
+        }
+        image[r][c] = newColor;
+    }
+}
+int main() {
+    // Write C++ code here
+    std::vector<std::vector<int>> image = {{1, 1, 1},{1, 1, 0}, {1, 0, 1}};
+    int sr = 1, sc = 1, newColor = 2;
+    
+    fillFlood2(image, sr, sc, image[sr][sc], newColor);
+    
+    for (auto &r : image) {
+        std::cout << "[";
+        for (auto &c : r) {
+            std::cout << c << " ";
+        }
+        std::cout << "]\n";
+    }
+
+    return 0;
+}
