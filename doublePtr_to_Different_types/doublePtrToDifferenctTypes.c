4d13bb1979b587180c75297e56c0e9b5507761fb
maximum428 <35505967+maximum428@users.noreply.github.com>
2025-03-14 15:41:53 -0700

    Update doublePtrToDifferenctTypes.c

--- doublePtr_to_Different_types/doublePtrToDifferenctTypes.c
+++ doublePtr_to_Different_types/doublePtrToDifferenctTypes.c
@@ -13,29 +13,42 @@ void doublePtrToDifferentTypes() {
 }
 
 void demo() {
-    int a = 12;
-    void* int_ptr = &a;
-    a = 13;
-    void *v_ptr = int_ptr;
-    *(int*)v_ptr = 14;
-    void **vv_ptr = &int_ptr;
-    *(int*)*vv_ptr = 15;
-    void ***vvv_ptr = &vv_ptr;
-    *(int*)**vvv_ptr = 16;
-    
-    cout << *(int*)v_ptr << endl;
-    cout << *(int*)*vv_ptr << endl;
-    cout << *(int*)**vvv_ptr << endl;
-    
-    floa b = 3.2;
-    void *float_ptr = &b;
-    v_ptr = float_ptr;
-    vv_ptr = &v_ptr;
-    
-    cout << *(float*)float_ptr << endl;
-    cout << *(float*)v_ptr << endl;
-    cout << *(float*)*vv_ptr << endl;
-    cout << *(float*)**vvv_ptr << endl;
+  int a = 12;
+  int* int_ptr = &a;
+  printf("%d\n", *int_ptr);
+
+  a = 13;
+  printf("%d\n", *int_ptr);
+
+  void* v_ptr = int_ptr;
+  *(int*)v_ptr = 14;
+  printf("%d\n", *int_ptr);
+  printf("%d\n", a);
+  printf("%d\n", *(int*)v_ptr);
+
+  void **vv_ptr = (void*)&int_ptr;  // or &v_ptr
+  *(int*)*vv_ptr = 15;
+  printf("%d\n", *(int*)*vv_ptr);
+  printf("%d\n", *(int*)v_ptr);
+  printf("%d\n", *int_ptr);
+  printf("%d\n", a);
+
+  void ***vvv_ptr = &vv_ptr;
+  *(int*)**vvv_ptr = 16;
+  printf("%d\n", *(int*)**vvv_ptr);
+  printf("%d\n", *(int*)*vv_ptr);
+  printf("%d\n", *(int*)v_ptr);
+  printf("%d\n", *int_ptr);
+  printf("%d\n", a);
+
+  float b = 3.2;
+  float* float_ptr = &b;
+  printf("%.2f\n", *float_ptr);
+  v_ptr = float_ptr;
+  printf("%.2f\n", *(float*)v_ptr);
+  vv_ptr = &v_ptr;
+  printf("%.2f\n", *(float*)*vv_ptr);
+  printf("%.2f\n", *(float*)**vvv_ptr);
 }
 
 int main(void) {
