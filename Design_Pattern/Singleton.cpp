947e3d57a151dd126067bccf75fe6c125cf3c588
maximum428 <35505967+maximum428@users.noreply.github.com>
2025-12-27 14:18:42 +0800

    Update Singleton.cpp

--- Design_Pattern/Singleton.cpp
+++ Design_Pattern/Singleton.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
-#include <mutex>
 #include <chrono>
 #include <future>
-#include <thread>
-
-constexpr auto tenMill = 10000000;
+#include <mutex>
 
 using namespace std;
+
+constexpr auto tenMill = 10000000;
 
 class MySingleton {
 public:
     static MySingleton& getInstance() {
-        std::call_once(initInstanceFlag, &MySingleton::initSingleton);
-        return *instance;
+        //call_once(initInstanceFlag, &MySingleton::initSingleton);
+        static MySingleton instance;
+        return instance;
     }
 private:
     MySingleton() = default;
-    //~MySingleton() = default;
-    //MySingleton(const MySingleton &) = delete;
-    MySingleton& operator=(const MySingleton &) = delete;
-
+    MySingleton(const MySingleton&) = delete;
+    MySingleton& operator=(const MySingleton&) = delete;
+    
     static MySingleton *instance;
-    static std::once_flag initInstanceFlag;
+    static once_flag initInstanceFlag;
     static void initSingleton() {
         instance = new MySingleton();
     }
 };
 
 MySingleton *MySingleton::instance = nullptr;
-std::once_flag MySingleton::initInstanceFlag;
+once_flag MySingleton::initInstanceFlag;
 
-std::chrono::duration<double> getTime() {
-    auto begin = chrono::system_clock::now();
+chrono::duration<double> getTime() {
+    auto begin = chrono::steady_clock::now();
     for (size_t i = 0; i < tenMill; i++) {
         MySingleton::getInstance();
     }
-    return chrono::system_clock::now() - begin;
+    return chrono::steady_clock::now() - begin;
 }
 
 class Singleton {
 public:
     static Singleton *GetInstance() {
-        std::lock_guard<std::mutex> guard(mutex);
-        if (instance == 0) {
+        lock_guard guard(m_mutex);
+        if (instance == nullptr) {
             instance = new Singleton();
         }
         return instance;
@@ -51,23 +50,22 @@ private:
     Singleton() {}
     virtual ~Singleton() {}
     static Singleton *instance;
-    static std::mutex mutex;
+    static mutex m_mutex;
 };
 
-Singleton *Singleton::instance = 0;
-std::mutex Singleton::mutex;
+Singleton *Singleton::instance = nullptr;
+mutex Singleton::m_mutex;
 
-int main() {
+int main(int argc, char** argv) {
     Singleton *singleton = Singleton::GetInstance();
     
-    auto fut1= std::async(std::launch::async,getTime);
-    auto fut2= std::async(std::launch::async,getTime);
-    auto fut3= std::async(std::launch::async,getTime);
-    auto fut4= std::async(std::launch::async,getTime);
+    auto future1 = async(launch::async, getTime);
+    auto future2 = async(launch::async, getTime);
+    auto future3 = async(launch::async, getTime);
+    auto future4 = async(launch::async, getTime);
     
-    auto total= fut1.get() + fut2.get() + fut3.get() + fut4.get();
+    auto total = future1.get() + future2.get() + future3.get() + future4.get();
     
-    std::cout << total.count() << std::endl;
-    
+    cout << total.count() << endl;
     return 0;
 }
