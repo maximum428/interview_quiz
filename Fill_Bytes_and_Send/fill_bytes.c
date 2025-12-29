bbbcf73456d5eda71919a8e3a44d1c14e03a1315
maximum428 <35505967+maximum428@users.noreply.github.com>
2025-12-30 03:58:39 +0800

    Refactor receive function for byte handling
    Fix bugs in receive function and improve buffer handling.

--- Fill_Bytes_and_Send/fill_bytes.c
+++ Fill_Bytes_and_Send/fill_bytes.c
@@ -1,6 +1,28 @@
 #include <stdiolh>
 #include <stdlib.h>
 
+#define MIN(x, y)   x ^ ((x ^ y) & -(x < y))
+
+void receive(const uint8_t *bytes, size_t nbytes) {
+    static uint8_t buffer[16];
+    static size_t curlength = 0;
+    
+    while (nbytes > 0) {
+        size_t to_copy = MIN(16 - curlength, nbytes);
+        memcpy(buffer + currlength, bytes, to_copy);
+        
+        curlength += to_copy;
+        bytes += to_copy;
+        nbytes -= to_copy;
+        
+        if (curlength == 16) {
+            // send(buffer);
+            printf("Sedn 16 bytes\n");
+            curlength = 0;
+        }
+    }
+}
+
 void method(FILE *fd, Data *data) {
     size_t total = 0;
     unsigned char *ptr = (unsigned char*)data;
