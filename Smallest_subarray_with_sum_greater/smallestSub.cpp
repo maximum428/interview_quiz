fd7e943a588c6788bd6460b8215bdc1cbf2d8477
maximum428 <35505967+maximum428@users.noreply.github.com>
2026-01-01 13:05:43 +0800

    Refactor smallest subarray function implementation
    Refactor the smallest subarray function to handle edge cases and improve readability.

--- Smallest_subarray_with_sum_greater/smallestSub.cpp
+++ Smallest_subarray_with_sum_greater/smallestSub.cpp
@@ -26,3 +26,37 @@ int main() {
     vector<int> vec = { 1, 4, 45, 6, 10, 19 };
     cout << smallestSubarray(vec, 51) << endl;
 }
+
+=================================================================
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int smallestSubarrayWithSum(const vector<int>& nums, int x) {
+    int n = nums.size(), minLen = INT_MAX, sum = 0, start = 0;
+    
+    for (int end = 0; end < n; end++) {
+        sum += nums[end];
+        
+        while (sum > x) {
+            minLen = min(minLen, end - start + 1);
+            sum -= nums[start++];
+        }
+    }
+    return (minLen == INT_MAX) ?  -1 : minLen;
+}
+
+int main() {
+    vector<int> nums = {1, 4, 45, 6, 0, 19};
+    int x = 51;
+    
+    int res = smallestSubarrayWithSum(nums, x);
+    if (res == -1) {
+        cout << "Not Possible\n";
+    } else {
+        cout << res << endl;
+    }
+    return 0;
+}
