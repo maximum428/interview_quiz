52188be44025260df5fa1fee34dbb7fc12d8a5d7
maximum428 <35505967+maximum428@users.noreply.github.com>
2026-01-10 10:19:55 -0800

    Refactor burger classes to use smart pointers

--- Design_Pattern/AbstractFactory_Burger.cpp
+++ Design_Pattern/AbstractFactory_Burger.cpp
@@ -286,3 +286,286 @@ int main() {
     delete burger;
     delete factory;
 }
+
+/************************************************************************/
+/************  Smart Pointer Version **************/
+/************************************************************************/
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Bun {
+public:
+    virtual ~Bun() {}
+    virtual void Prepare() = 0;
+};
+
+class Pattie {
+public:
+    virtual ~Pattie() {}
+    virtual void Prepare() = 0;
+};
+
+class Pickle {
+public:
+    virtual ~Pickle() {}
+    virtual void Prepare() = 0;
+};
+
+class Onions {
+public:
+    virtual ~Onions() {}
+    virtual void Prepare() = 0;
+};
+
+class Ketchup {
+public:
+    virtual ~Ketchup() {}
+    virtual void Prepare() = 0;
+};
+
+class Mustard {
+public:
+    virtual ~Mustard() {}
+    virtual void Prepare() = 0;
+};
+
+class SesameSeedBun : public Bun {
+public:
+    SesameSeedBun() {}
+    virtual ~SesameSeedBun() {}
+    virtual void Prepare() {
+        cout << "Preparing Sesame Seed Bun.\n";
+    }
+};
+
+class SeedlessBun : public Bun {
+public:
+    SeedlessBun() {}
+    virtual ~SeedlessBun() {}
+    virtual void Prepare() {
+        cout << "Preparing Seedless Bun.\n";
+    }
+};
+
+class FlameBroiledPattie : public Pattie {
+public:
+    FlameBroiledPattie() {}
+    virtual ~FlameBroiledPattie() {}
+    virtual void Prepare() {
+        cout << "Preparing Flame Broiled Burger.\n";
+    }
+};
+
+class GrilledPattie : public Pattie {
+public:
+    GrilledPattie() {}
+    virtual ~GrilledPattie() {}
+    virtual void Prepare() {
+        cout << "Preparing Grilled Pattie.\n";
+    }
+};
+
+class SweetPickles : public Pickle {
+public:
+    SweetPickles() {}
+    virtual ~SweetPickles() {}
+    virtual void Prepare() {
+        cout << "Preparing Sweet Pickles.\n";
+    }
+};
+
+class SourDillPickles : public Pickle {
+public:
+    SourDillPickles() {}
+    virtual ~SourDillPickles() {}
+    virtual void Prepare() {
+        cout << "Preparing Sour Dill Pickles.\n";
+    }
+};
+
+class NoOnions : public Onions {
+public:
+    NoOnions() {}
+    virtual ~NoOnions() {}
+    virtual void Prepare() {
+        // do nothing
+    }
+};
+
+class WhiteOnions : public Onions {
+public:
+    WhiteOnions() {}
+    virtual ~WhiteOnions() {}
+    virtual void Prepare() {
+        cout << "Preparing White Onions.\n";
+    }
+};
+
+class HeinzKetchup : public Ketchup {
+public:
+    HeinzKetchup() {}
+    virtual ~HeinzKetchup() {}
+    virtual void Prepare() {
+        cout << "Preparing heinx Ketchup.\n";
+    }
+};
+
+class McDonaldKetchup : public Ketchup {
+public:
+    McDonaldKetchup() {}
+    virtual ~McDonaldKetchup() {}
+    virtual void Prepare() {
+        cout << "Preparing McDonalds Ketchup.\n";
+    }
+};
+
+class HeinzMustard : public Mustard {
+public:
+    HeinzMustard() {}
+    virtual ~HeinzMustard() {}
+    virtual void Prepare() {
+        cout << "Preparing Heinz Mustard.\n";
+    }
+};
+
+class McDonaldMustard : public Mustard {
+public:
+    McDonaldMustard() {}
+    virtual ~McDonaldMustard() {}
+    virtual void Prepare() {
+        cout << "Preparing McDonalds Mustard.\n";
+    }
+};
+
+class BurgerIngredientFactory {
+public:
+    virtual ~BurgerIngredientFactory() = default;
+    virtual unique_ptr<Bun> CreateBun() = 0;
+    virtual unique_ptr<Pattie> CreatePattie() = 0;
+    virtual unique_ptr<Pickle> CreatePickle() = 0;
+    virtual unique_ptr<Onions> CreateOnions() = 0;
+    virtual unique_ptr<Ketchup> CreateKetchup() = 0;
+    virtual unique_ptr<Mustard> CreateMustard() = 0;
+};
+
+class BurgerKingBurgerIngredientFactory : public BurgerIngredientFactory {
+public:
+    unique_ptr<Bun> CreateBun() override {
+        return make_unique<SesameSeedBun>();
+    }
+    unique_ptr<Pattie> CreatePattie() override {
+        return make_unique<FlameBroiledPattie>();
+    }
+    unique_ptr<Pickle> CreatePickle() override {
+        return make_unique<SweetPickles>();
+    }
+    unique_ptr<Onions> CreateOnions() override {
+        return make_unique<NoOnions>();
+    }
+    unique_ptr<Ketchup> CreateKetchup() override {
+        return make_unique<HeinzKetchup>();
+    }
+    unique_ptr<Mustard> CreateMustard() override {
+        return make_unique<HeinzMustard>();
+    }
+};
+
+class McDonaldBurgerIngredientFactory : public BurgerIngredientFactory {
+public:
+    unique_ptr<Bun> CreateBun() override {
+        return make_unique<SeedlessBun>();
+    }
+    unique_ptr<Pattie> CreatePattie() override {
+        return make_unique<GrilledPattie>();
+    }
+    unique_ptr<Pickle> CreatePickle() override {
+        return make_unique<SourDillPickles>();
+    }
+    unique_ptr<Onions> CreateOnions() override {
+        return make_unique<WhiteOnions>();
+    }
+    unique_ptr<Ketchup> CreateKetchup() override {
+        return make_unique<McDonaldKetchup>();
+    }
+    unique_ptr<Mustard> CreateMustard() override {
+        return make_unique<McDonaldMustard>();
+    }
+};
+
+class Burger {
+public:
+    virtual ~Burger() {}
+    virtual void Prepare() = 0;
+};
+
+class BurgerKingBurger : public Burger {
+    unique_ptr<Bun> m_Bun;
+    unique_ptr<Pattie> m_Pattie;
+    unique_ptr<Pickle> m_Pickles;
+    unique_ptr<Onions> m_Onions;
+    unique_ptr<Ketchup> m_Ketchup;
+    unique_ptr<Mustard> m_Mustard;
+public:
+    BurgerKingBurger(BurgerIngredientFactory& factory) : m_Bun(factory.CreateBun()),
+                                                         m_Pattie(factory.CreatePattie()),
+                                                         m_Pickles(factory.CreatePickle()),
+                                                         m_Onions(factory.CreateOnions()),
+                                                         m_Ketchup(factory.CreateKetchup()),
+                                                         m_Mustard(factory.CreateMustard()) {}
+    virtual ~BurgerKingBurger() {}
+    virtual void Prepare() {
+        cout << "Preparing Burger King Burger.\n";
+        m_Bun->Prepare();
+        m_Pattie->Prepare();
+        m_Pickles->Prepare();
+        m_Onions->Prepare();
+        m_Ketchup->Prepare();
+        m_Mustard->Prepare();
+    }
+};
+
+class McDonaldBurger : public Burger {
+    unique_ptr<Bun> m_Bun;
+    unique_ptr<Pattie> m_Pattie;
+    unique_ptr<Pickle> m_Pickles;
+    unique_ptr<Onions> m_Onions;
+    unique_ptr<Ketchup> m_Ketchup;
+    unique_ptr<Mustard> m_Mustard;
+public:
+    McDonaldBurger(BurgerIngredientFactory& factory) : m_Bun(factory.CreateBun()),
+                                                       m_Pattie(factory.CreatePattie()),
+                                                       m_Pickles(factory.CreatePickle()),
+                                                       m_Onions(factory.CreateOnions()),
+                                                       m_Ketchup(factory.CreateKetchup()),
+                                                       m_Mustard(factory.CreateMustard()) {}
+    virtual ~McDonaldBurger() {}
+    virtual void Prepare() {
+        cout << "Preparing McDonalds Burger.\n";
+        m_Bun->Prepare();
+        m_Pattie->Prepare();
+        m_Pickles->Prepare();
+        m_Onions->Prepare();
+        m_Ketchup->Prepare();
+        m_Mustard->Prepare();
+    }
+};
+
+int main(int argc, char** argv) {
+    {
+        auto factory = make_unique<BurgerKingBurgerIngredientFactory>();
+        auto burger = make_unique<BurgerKingBurger>(*factory);
+        burger->Prepare();
+    }
+    
+    cout << "\n";
+    
+    {
+        auto factory = make_unique<McDonaldBurgerIngredientFactory>();
+        auto burger = make_unique<McDonaldBurger>(*factory);
+        burger->Prepare();
+    }
+    
+    return 0;
+}
