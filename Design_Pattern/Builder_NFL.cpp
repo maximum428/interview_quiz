56b6cf722318b57cae6b2882109cd2c0a23c3afa
maximum428 <35505967+maximum428@users.noreply.github.com>
2026-01-10 14:52:15 -0800

    Update Builder_NFL.cpp

--- Design_Pattern/Builder_NFL.cpp
+++ Design_Pattern/Builder_NFL.cpp
@@ -191,3 +191,166 @@ int main() {
     delete gameScheduleBuilder;
     return 0;
 }
+
+
+/*********************************************************************************
+************************ Smart Pointer Version ************************
+**********************************************************************************/
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <memory>
+
+using namespace std;
+using namespace std::string_literals;
+
+class Game {
+    string m_time;
+    string m_homeTeam;
+    string m_awayTeam;
+public:
+    void SetTime(const string& time) { m_time = time; }
+    void SetHomeTeam(const string& name) { m_homeTeam = name; }
+    void SetAwayTeam(const string& name) { m_awayTeam = name; }
+    
+    string GetTime() const { return m_time; }
+    string GetHomeTeam() const { return m_homeTeam; }
+    string GetAwayTeam() const { return m_awayTeam; }
+    void Display() const {
+        cout << m_time << " : " << m_awayTeam << " at " << m_homeTeam << endl;
+    }
+};
+
+class GameDate {
+    string m_date;
+    map<unsigned int, unique_ptr<Game>> m_games;
+public:
+    explicit GameDate(string date) : m_date(move(date)) {}
+    void AddGame(unsigned int gameNumber) {
+        m_games.emplace(gameNumber, make_unique<Game>());
+    }
+    Game* GetGame(unsigned int gameNumber) {
+        auto it = m_games.find(gameNumber);
+        return it != m_games.end() ? it->second.get() : nullptr;
+    }
+    void Display() const {
+        cout << m_date << "\n\n";
+        for (const auto& [_, game] : m_games) {
+            game->Display();
+        }
+    }
+};
+
+class GameSchedule {
+public:
+    virtual ~GameSchedule() = default;
+    virtual void Display() const = 0;
+};
+
+class SportsGameSchedule : public GameSchedule {
+    map<unsigned int, unique_ptr<GameDate>> m_gameDates;
+public:
+    void AddGameDate(unsigned int dateNumber, string date) {
+        m_gameDates.emplace(dateNumber, make_unique<GameDate>(move(date)));
+    }
+    GameDate *GetGameDate(unsigned int dateNumber) {
+        auto it = m_gameDates.find(dateNumber);
+        return it != m_gameDates.end() ? it->second.get() : nullptr;
+    }
+    void Display() const override {
+        for (const auto& [_, date] : m_gameDates) {
+            date->Display();
+            cout << "\n";
+        }
+    }
+};
+
+class GameScheduleBuilder {
+public:
+    virtual ~GameScheduleBuilder() = default;
+    virtual void BuildSchedule() = 0;
+    virtual void BuildDate(unsigned int, string) = 0;
+    virtual void BuildGame(unsigned int, unsigned int) = 0;
+    virtual void AddTime(unsigned int, unsigned int, string) = 0;
+    virtual void AddHomeTeam(unsigned int, unsigned int, string) = 0;
+    virtual void AddAwayTeam(unsigned int, unsigned int, string) = 0;
+};
+
+class SportsGameScheduleBuilder : public GameScheduleBuilder {
+    unique_ptr<SportsGameSchedule> m_gameSchedule;
+public:
+    void BuildSchedule() override {
+        m_gameSchedule = make_unique<SportsGameSchedule>();
+    }
+    void BuildDate(unsigned int dateNumber, string date) override {
+        m_gameSchedule->AddGameDate(dateNumber, move(date));
+    }
+    void BuildGame(unsigned int dateNumber, unsigned int gameNumber) override {
+        m_gameSchedule->GetGameDate(dateNumber)->AddGame(gameNumber);
+    }
+    void AddTime(unsigned int dateNumber, unsigned int gameNumber, string time) override {
+        m_gameSchedule->GetGameDate(dateNumber)->GetGame(gameNumber)->SetTime(move(time));
+    }
+    void AddHomeTeam(unsigned int dateNumber, unsigned int gameNumber, string name) override {
+        m_gameSchedule->GetGameDate(dateNumber)->GetGame(gameNumber)->SetHomeTeam(move(name));
+    }
+    void AddAwayTeam(unsigned int dateNumber, unsigned int gameNumber, string name) override {
+        m_gameSchedule->GetGameDate(dateNumber)->GetGame(gameNumber)->SetAwayTeam(move(name));
+    }
+    GameSchedule& GetSchedule() {
+        return *m_gameSchedule;
+    }
+};
+
+class NFLScheduler {
+    GameScheduleBuilder& m_gameScheduleBuilder;
+public:
+    explicit NFLScheduler(GameScheduleBuilder& builder) : m_gameScheduleBuilder(builder) {}
+    void ConstructSchedule() {
+        unsigned int dateNumber = 0;
+        unsigned int gameNumber = 0;
+        
+        m_gameScheduleBuilder.BuildSchedule();
+        m_gameScheduleBuilder.BuildDate(dateNumber, "09/07/17"s);
+        m_gameScheduleBuilder.BuildGame(dateNumber, gameNumber);
+        m_gameScheduleBuilder.AddTime(dateNumber, gameNumber, "8:30pm"s);
+        m_gameScheduleBuilder.AddHomeTeam(dateNumber, gameNumber, "New England Patriots"s);
+        m_gameScheduleBuilder.AddAwayTeam(dateNumber, gameNumber, "Kansas City Chiefs"s);
+        
+        m_gameScheduleBuilder.BuildDate(++dateNumber, "09/10/17"s);
+        m_gameScheduleBuilder.BuildGame(dateNumber, ++gameNumber);
+        m_gameScheduleBuilder.AddTime(dateNumber, gameNumber, "1:00pm"s);
+        m_gameScheduleBuilder.AddHomeTeam(dateNumber, gameNumber, "New York Jets"s);
+        m_gameScheduleBuilder.AddAwayTeam(dateNumber, gameNumber, "Buffalo Bills"s);
+        
+        m_gameScheduleBuilder.BuildGame(dateNumber, ++gameNumber);
+        m_gameScheduleBuilder.AddTime(dateNumber, gameNumber, "1:00pm"s);
+        m_gameScheduleBuilder.AddHomeTeam(dateNumber, gameNumber, "Washington Redskins"s);
+        m_gameScheduleBuilder.AddAwayTeam(dateNumber, gameNumber, "Philadelphia Eagles"s);
+        
+        m_gameScheduleBuilder.BuildGame(dateNumber, ++gameNumber);
+        m_gameScheduleBuilder.AddTime(dateNumber, gameNumber, "1:00pm"s);
+        m_gameScheduleBuilder.AddHomeTeam(dateNumber, gameNumber, "Tennessee Titans"s);
+        m_gameScheduleBuilder.AddAwayTeam(dateNumber, gameNumber, "Las Vegas Raiders"s);
+        
+        m_gameScheduleBuilder.BuildGame(dateNumber, ++gameNumber);
+        m_gameScheduleBuilder.AddTime(dateNumber, gameNumber, "1:00pm"s);
+        m_gameScheduleBuilder.AddHomeTeam(dateNumber, gameNumber, "Miami Dolphins"s);
+        m_gameScheduleBuilder.AddAwayTeam(dateNumber, gameNumber, "Tampa Bay Buccaneers"s);
+        
+        m_gameScheduleBuilder.BuildGame(dateNumber, ++gameNumber);
+        m_gameScheduleBuilder.AddTime(dateNumber, gameNumber, "1:00pm"s);
+        m_gameScheduleBuilder.AddHomeTeam(dateNumber, gameNumber, "Houston Texans"s);
+        m_gameScheduleBuilder.AddAwayTeam(dateNumber, gameNumber, "Jacksonville Jaguars"s);
+    }
+};
+
+int main(int argc, char** argv) {
+    SportsGameScheduleBuilder gameScheduleBuilder;
+    NFLScheduler scheduler(gameScheduleBuilder);
+    scheduler.ConstructSchedule();
+    gameScheduleBuilder.GetSchedule().Display();
+
+    return 0;
+}
