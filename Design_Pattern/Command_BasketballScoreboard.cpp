9d4a2d0c02dacd806b6c701dc8f52eabde1a1290
maximum428 <35505967+maximum428@users.noreply.github.com>
2026-01-10 18:50:24 -0800

    Update Command_BasketballScoreboard.cpp

--- Design_Pattern/Command_BasketballScoreboard.cpp
+++ Design_Pattern/Command_BasketballScoreboard.cpp
@@ -256,3 +256,213 @@ int main() {
     delete threePointFieldGoalCommandAwayTeam;
     return 0;
 }
+
+
+/********************************************************************************************
+*********************** Smart Pointer version *******************************
+*********************************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <deque>
+#include <memory>
+#include <string>
+
+using namespace std;
+using namespace std::string_literals;
+
+class Command {
+public:
+    virtual ~Command() = default;
+    virtual void Execute() = 0;
+    virtual void Undo() = 0;
+};
+
+class BasketballScoreboardTeamObserver {
+public:
+    virtual ~BasketballScoreboardTeamObserver() = default;
+    virtual void TeamUpdate() = 0;
+};
+
+class BasketballScoreTeamSubject {
+public:
+    virtual ~BasketballScoreTeamSubject() = default;
+    virtual void Subscribe(BasketballScoreboardTeamObserver*) = 0;
+    virtual void Unsubscribe(BasketballScoreboardTeamObserver*) = 0;
+    virtual void Notify() = 0;
+};
+
+class BasketballScoreboardTeam : public BasketballScoreTeamSubject {
+    string m_name;
+    int m_score{0};
+    vector<BasketballScoreboardTeamObserver*> m_observers;
+public:
+    explicit BasketballScoreboardTeam(string name) : m_name(move(name)) {}
+    void Subscribe(BasketballScoreboardTeamObserver *observer) override {
+        m_observers.push_back(observer);
+    }
+    void Unsubscribe(BasketballScoreboardTeamObserver *observer) override {
+        m_observers.erase(
+            remove(m_observers.begin(), m_observers.end(), observer), m_observers.end()
+        );
+    }
+    void Notify() override {
+        for (auto* o : m_observers)
+            o->TeamUpdate();
+    }
+                                                            
+    const string& GetName() const {
+        return m_name;
+    }
+    int GetScore() const {
+        return m_score;
+    }
+    void FreeThrowMade() {
+        cout << GetName() << " : free throw made            | ";
+        ScoringUpdate(1);
+    }
+    void WaiveOffFreeThrow() {
+        cout << GetName() << " : free throw waived of           | ";
+        ScoringUpdate(-1);
+    }
+    void TwoPointFieldGoalMade() {
+        cout << GetName() << " : 2 point field goal made            | ";
+        ScoringUpdate(2);
+    }
+    void WaiveOffTwoPointFieldGoalMade() {
+        cout << GetName() << " : 2 point field goal waived off          | ";
+        ScoringUpdate(-2);
+    }
+    void ThreePointFieldGoalMade() {
+        cout << GetName() << " : 3 point field goal made            | ";
+        ScoringUpdate(3);
+    }
+    void WaiveOffThreePointFieldGoalMade() {
+        cout << GetName() << " : 3 point field goal waived off            | ";
+        ScoringUpdate(-3);
+    }
+    void Display() {
+        cout << m_name << " : " << m_score << endl;
+    }
+    void ScoringUpdate(int additionalPoints) {
+        m_score =max(0, m_score + additionalPoints);
+        Notify();
+    }
+};
+
+class BasketballScoreboard : public BasketballScoreboardTeamObserver {
+    BasketballScoreboardTeam *m_awayTeam, *m_homeTeam;
+public:
+    BasketballScoreboard(BasketballScoreboardTeam *awayTeam, BasketballScoreboardTeam *homeTeam) : m_awayTeam(awayTeam), m_homeTeam(homeTeam) {
+        m_awayTeam->Subscribe(this);
+        m_homeTeam->Subscribe(this);
+    }
+    void TeamUpdate() override {
+        cout << "Score : " << m_awayTeam->GetName() << " " << m_awayTeam->GetScore() << ", " << m_homeTeam->GetName() << " " << m_homeTeam->GetScore() << endl;
+    }
+};
+
+class FreeThrowCommand : public Command {
+    BasketballScoreboardTeam *m_Team;
+public:
+    explicit FreeThrowCommand(BasketballScoreboardTeam *team) : m_Team(team) {}
+    void Execute() override {
+        m_Team->FreeThrowMade();
+    }
+    void Undo() override {
+        m_Team->WaiveOffFreeThrow();
+    }
+};
+
+class TwoPointFieldGoalCommand : public Command {
+    BasketballScoreboardTeam *m_Team;
+public:
+    explicit TwoPointFieldGoalCommand(BasketballScoreboardTeam *team) : m_Team(team) {}
+    void Execute() override {
+        m_Team->TwoPointFieldGoalMade();
+    }
+    void Undo() override {
+        m_Team->WaiveOffTwoPointFieldGoalMade();
+    }
+};
+
+class ThreePointFieldGoalCommand : public Command {
+    BasketballScoreboardTeam *m_Team;
+public:
+    explicit ThreePointFieldGoalCommand(BasketballScoreboardTeam *team) : m_Team(team) {}
+    void Execute() override {
+        m_Team->ThreePointFieldGoalMade();
+    }
+    void Undo() override {
+        m_Team->WaiveOffThreePointFieldGoalMade();
+    }
+};
+
+enum BasketballScoreboardButtonId {
+    BSCI_AwayTeamFreeThrowButtonId,
+    BSCI_AwayTeamTwoPointFieldGoalButtonId,
+    BSCI_AwayTeamThreePointFieldGoalButtonId,
+    BSCI_HomeTeamFreeThrowButtonId,
+    BSCI_HomeTeamTwoPointFieldGoalButtonId,
+    BSCI_HomeTeamThreePointFieldGoalButtonId
+};
+
+class BasketballScoreboardControl {
+    map<BasketballScoreboardButtonId, unique_ptr<Command>> m_commandMap;
+    deque<Command *> m_commandHistory;
+public:
+    void SetCommand(BasketballScoreboardButtonId buttonId, unique_ptr<Command> command) {
+        m_commandMap.emplace(buttonId, move(command));
+    }
+    void ButtonPressed(BasketballScoreboardButtonId buttonId) {
+        auto it = m_commandMap.find(buttonId);
+        if (it != m_commandMap.end()) {
+            it->second->Execute();
+            m_commandHistory.push_front(it->second.get());
+        }
+    }
+    void UndoLastButtonPress() {
+        if (!m_commandHistory.empty()) {
+            m_commandHistory.front()->Undo();
+            m_commandHistory.pop_front();
+        }
+    }
+};
+
+int main(int argc, char** argv) {
+    cout << "Basketball Scoreboard\n";
+    auto homeTeam = make_unique<BasketballScoreboardTeam>("Home Team"s);
+    auto awayTeam = make_unique<BasketballScoreboardTeam>("Away Team"s);
+    BasketballScoreboard scoreBoard(awayTeam.get(), homeTeam.get());
+    
+    auto basketballScoreboardControl = make_unique<BasketballScoreboardControl>();
+    
+    basketballScoreboardControl->SetCommand(BSCI_HomeTeamFreeThrowButtonId, make_unique<FreeThrowCommand>(homeTeam.get()));
+    basketballScoreboardControl->SetCommand(BSCI_HomeTeamTwoPointFieldGoalButtonId, make_unique<TwoPointFieldGoalCommand>(homeTeam.get()));
+    basketballScoreboardControl->SetCommand(BSCI_HomeTeamThreePointFieldGoalButtonId, make_unique<ThreePointFieldGoalCommand>(homeTeam.get()));
+    
+    basketballScoreboardControl->SetCommand(BSCI_AwayTeamFreeThrowButtonId, make_unique<FreeThrowCommand>(awayTeam.get()));
+    basketballScoreboardControl->SetCommand(BSCI_AwayTeamTwoPointFieldGoalButtonId, make_unique<TwoPointFieldGoalCommand>(awayTeam.get()));
+    basketballScoreboardControl->SetCommand(BSCI_AwayTeamThreePointFieldGoalButtonId, make_unique<ThreePointFieldGoalCommand>(awayTeam.get()));
+    
+    basketballScoreboardControl->ButtonPressed(BSCI_HomeTeamFreeThrowButtonId);
+    basketballScoreboardControl->ButtonPressed(BSCI_HomeTeamTwoPointFieldGoalButtonId);
+    basketballScoreboardControl->ButtonPressed(BSCI_HomeTeamThreePointFieldGoalButtonId);
+    
+    basketballScoreboardControl->UndoLastButtonPress();
+    basketballScoreboardControl->UndoLastButtonPress();
+    basketballScoreboardControl->UndoLastButtonPress();
+    
+    basketballScoreboardControl->ButtonPressed(BSCI_AwayTeamFreeThrowButtonId);
+    basketballScoreboardControl->ButtonPressed(BSCI_AwayTeamTwoPointFieldGoalButtonId);
+    basketballScoreboardControl->ButtonPressed(BSCI_AwayTeamThreePointFieldGoalButtonId);
+    
+    basketballScoreboardControl->UndoLastButtonPress();
+    basketballScoreboardControl->UndoLastButtonPress();
+    basketballScoreboardControl->UndoLastButtonPress();
+    
+    return 0;
+}
+
+
